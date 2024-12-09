454cf941f173892f788f292a0740cedffb914868
ThunderRabbit <zuzazibubabi@gmail.com>
2024-12-09 10:25:59 +0800

    added some filtering functionality

--- gameSearcher.js
+++ gameSearcher.js
@@ -3,7 +3,12 @@ const submitButton = document.getElementById('submitButton');
 
 const pagesContainer = document.querySelector('.pagesContainer');
 const apiKey = '2c80b02fad8b4f65872780867579ae94';
-
+const submitByMonth = document.querySelector('.daysAgo');
+const lastWeek = document.querySelector('.lastWeek');
+const thisWeek = document.querySelector('.thisWeek');
+const bestYear = document.querySelector('.bestYear');
+const popular = document.querySelector('.popular');
+const allTime = document.querySelector('.allTime');
 /*
 async function fetchData() {
 
@@ -31,19 +36,90 @@ submitButton.addEventListener('click', () => {
 }) */
 
 
+    
 const searchGames = async(input) => {
 
     try {
-    const response = await(fetch)(`https://api.rawg.io/api/games?key=${apiKey}&search=${input}&page_size=10`);
-    const data = await response.json();
-    console.log(data)
-    displayGames(data.results); }
-
-    catch(error){
-        console.log('Error fetching data from RAWG API', error);
+        const response = await fetch(`https://api.rawg.io/api/games?ordering=-metacritic&key=${apiKey}&search=${input}&page_size=100`); 
+        const data = await response.json();
+        displayGames(data.results)
+        console.log(data.results);
+    }  
+    catch(error) {
+        console.error('error fetching from api')
     }
 
 }
+
+searchGames();
+
+const searchByLastMonth = async() => {
+    try {
+        const response =  await fetch(`https://api.rawg.io/api/games?ordering=-released&key=${apiKey}&date=2024-12-09&page_size=100`); 
+        const data = await response.json();
+        console.log(data)
+        displayGames(data.results); }
+    
+        catch(error){
+            console.log('Error fetching data from RAWG API', error);
+        }
+}
+
+const searchByLastWeek = async() => {
+    try {
+        const response =  await fetch(`https://api.rawg.io/api/games?ordering=released&key=${apiKey}&date=2024-12-02,2024-12-09&page_size=100`); 
+        const data = await response.json();
+        console.log(data)
+        displayGames(data.results); }
+    
+        catch(error){
+            console.log('Error fetching data from RAWG API', error);
+        }
+}
+
+const searchByBestYear = async() => {
+    try {
+        const response = await fetch(`https://api.rawg.io/api/games?ordering=-metacritic,-rating,-reviews_count&key=${apiKey}&dates=2023-12-09,2024-12-09&page_size=100`);
+        const data = await response.json();
+        console.log(data)
+        displayGames(data.results); }
+    
+        catch(error){
+            console.log('Error fetching data from RAWG API', error);
+        }
+}
+
+bestYear.addEventListener('click', ()=> {
+    searchByBestYear()
+})
+
+lastWeek.addEventListener('click', ()=> {
+    searchByLastWeek()
+})
+
+submitByMonth.addEventListener('click', ()=> {
+
+    searchByLastMonth()
+
+})
+
+thisWeek.addEventListener('click', ()=> {
+    searchByLastWeek()
+})
+/*
+for(i=0;i <12; i++) {
+    const gameCard = document.createElement('div');
+    gameCard.classList.add('gameCard');
+
+    gameCard.innerHTML = `
+    <img class="gameBackground" src="https://media.rawg.io/media/games/936/936f0ffac0b3c9f5c8d185f610ed2631.jpg">
+    <div class="gameInfo"><h3 class="gameName">Super Mario Galaxy</h3>
+    <h3 class="gameMetaRating">97</h3></div>
+
+    `
+    pagesContainer.appendChild(gameCard);
+
+} */
 
 const displayGames = (games) => {
 
@@ -55,8 +131,9 @@ const displayGames = (games) => {
         gameCard.classList.add('gameCard');
 
         gameCard.innerHTML = `
-        <h3>${game.name}</h3>
-        <img src="${game.background_image}">
+        <img class="gameBackground" src="${game.background_image}">
+        <div class="gameInfo"><h3 class="gameName">${game.name}</h3>
+        <h3 class="gameMetaRating">${game.rating}</h3></div>
 
         `
         pagesContainer.appendChild(gameCard);
@@ -69,6 +146,19 @@ const displayGames = (games) => {
 
 submitButton.addEventListener('click', ()=> {
     
-    searchGames(userInput.value);
+    searchGamesMetaCritic(userInput.value);
 })
+
+const searchGamesMetaCritic = async(input) => {
+
+    try {
+        const response = await fetch(`https://api.rawg.io/api/games?ordering=-metacritic&key=${apiKey}&search=${input}&page_size=100`); 
+        const data = await response.json();
+        displayGames(data.results)
+        console.log(data.results);
+    }  
+    catch(error) {
+        console.error('error fetching from api')
+    }
+}
 
